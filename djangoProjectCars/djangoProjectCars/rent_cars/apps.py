35e7aea9512a40e39059df9ee66ff04cf770951b
Mihaela Aleksandrova <105858813+MishaAleksandrova@users.noreply.github.com>
2024-02-07 10:52:27 +0200

    Add files via upload

---
+++ djangoProjectCars/djangoProjectCars/rent_cars/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RentCarsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djangoProjectCars.rent_cars'
---
+++ djangoProjectCars/djangoProjectCars/sale_cars/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SaleCarsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'djangoProjectCars.sale_cars'
